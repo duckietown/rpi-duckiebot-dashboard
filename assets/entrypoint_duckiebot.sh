76fb9e861a2939c50390af221c992a4eb66d4388
Andrea Daniele <andreadaniele@AndreadielesMBP.lan>
2019-03-16 20:49:17 -0700

    added `ros` package

--- assets/entrypoint_duckiebot.sh
+++ assets/entrypoint_duckiebot.sh
@@ -3,6 +3,9 @@
 # constants
 HOSTNAME=$(hostname)
 
+# clone or update the `ros` package
+git -C $COMPOSE_PACKAGES_DIR/ros pull origin master || git clone --depth 1 https://github.com/afdaniele/compose-pkg-ros $COMPOSE_PACKAGES_DIR/ros
+
 # clone or update the `duckietown` package
 git -C $COMPOSE_PACKAGES_DIR/duckietown pull origin master || git clone --depth 1 https://github.com/afdaniele/compose-pkg-duckietown $COMPOSE_PACKAGES_DIR/duckietown
 
@@ -13,9 +16,14 @@ git -C $COMPOSE_PACKAGES_DIR/aido pull origin master || git clone --depth 1 http
 git -C $COMPOSE_PACKAGES_DIR/aido_duckiebot pull origin master || git clone --depth 1 https://github.com/afdaniele/compose-pkg-aido-duckiebot $COMPOSE_PACKAGES_DIR/aido_duckiebot
 
 # configure \compose\
-python /var/www/html/configure.py --guest_default_page monitor --navbar_title $HOSTNAME --website_name "AIDO Duckiebot Dashboard" \
-    --login_enabled 0 --cache_enabled 1 --logo_white http://$HOSTNAME.local/data/aido/images/AIDO_logo_negative_navbar.svg \
-    --logo_black http://$HOSTNAME.local/data/aido/images/AIDO_logo_positive.svg
+python /var/www/html/configure.py \
+  --guest_default_page monitor \
+  --navbar_title $HOSTNAME \
+  --website_name "AIDO Duckiebot Dashboard" \
+  --login_enabled 0 \
+  --cache_enabled 0 \
+  --logo_white http://$HOSTNAME.local/data/aido/images/AIDO_logo_negative_navbar.svg \
+  --logo_black http://$HOSTNAME.local/data/aido/images/AIDO_logo_positive.svg
 
 # run base entrypoint
 /root/entrypoint.sh
